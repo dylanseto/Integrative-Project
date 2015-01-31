c6b7e2f1543ea17e3ee0628390725d931280902f
killerartist95 <killerartist95@f68a8921-f938-4f9a-abcf-801dcbd576a8>
2015-01-31 13:50:01 +0000

    - added a comment
    git-svn-id: https://subversion.assembla.com/svn/integrative-project/trunk@18 f68a8921-f938-4f9a-abcf-801dcbd576a8

--- src/Main/Constants.java
+++ src/Main/Constants.java
@@ -1,12 +1,12 @@
 package Main;
 
-public interface Constants 
+public interface Constants
 {
 	public final int ZERO = 0;
 	public final int ONE = 1;
-	
+
 	public final double GRAVITY = 9.80665;
-	
+
 	//Index of Refraction
 	public final double INDEX_REF_AIR = 1.0003;
 	public final double INDEX_REF_WATER = 1.333;
@@ -15,7 +15,8 @@ public interface Constants
 	public final double INDEX_REF_ZIRCON = 1.920;
 	public final double INDEX_REF_DIAMOND = 2.417;
 	public final double INDEX_REF_PYREX = 1.470;
-	
+
+	//Sizing ratios
 	public final double METER_RATIO = 1/1000;
 	public final double SCREEN_SPLIT_RATIO = 1/2;
 
