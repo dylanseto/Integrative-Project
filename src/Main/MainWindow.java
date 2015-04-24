1705ebc754e837c05660dd9536accec9643acf90
killerartist95 <killerartist95@f68a8921-f938-4f9a-abcf-801dcbd576a8>
2015-04-24 11:11:01 +0000

    - added style.css and set it up -> nothing's in it yet, but the program can find it now
    git-svn-id: https://subversion.assembla.com/svn/integrative-project/trunk@131 f68a8921-f938-4f9a-abcf-801dcbd576a8

--- src/Main/MainWindow.java
+++ src/Main/MainWindow.java
@@ -63,7 +63,9 @@ public class MainWindow extends Application
 
         mainWindow.getChildren().addAll(topSplitPane, bottomSplitPane);
 		
-		primaryStage.setScene(new Scene(new Group(mainWindow), 599, 590));
+                Scene scene = new Scene(new Group(mainWindow), 599, 590);
+                scene.getStylesheets().add("/res/style.css");
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
